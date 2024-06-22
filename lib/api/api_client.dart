d826b9095bd7b6ca24e4a3c581fc183618835ed9
golery <golery.team@gmail.com>
2024-06-22 15:21:53 -0400

    dev

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -9,7 +9,10 @@ class ApiClient {
 
   Future<http.Response> getRequest(String endpoint) async {
     final url = Uri.parse('$baseUrl$endpoint');
-    return await http.get(url, headers: {"Authorization": "Bearer mock_token"});
+    return await http.get(url, headers: {
+      "Authorization": "Bearer mock_token",
+      "Accept": "application/json"
+    });
   }
 
   Future<http.Response> postRequest(String endpoint, dynamic body) async {
