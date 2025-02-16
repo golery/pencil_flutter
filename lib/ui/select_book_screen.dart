6597ab89c5bff42306ace6e8bf1d57e932bd86c1
golery <golery.team@gmail.com>
2025-02-16 14:37:08 -0500

    e3f

--- lib/ui/select_book_screen.dart
+++ lib/ui/select_book_screen.dart
@@ -14,21 +14,33 @@ class SelectBookScreen extends StatefulWidget {
 class _SelectBookScreenState extends State<SelectBookScreen> {
   Widget getBody(DataProvider dataProvider) {
     var books = dataProvider.bookList ?? [];
+
     return ListView.builder(
       itemCount: books.length,
       itemBuilder: (context, index) {
-        return Card(
-          clipBehavior: Clip.hardEdge,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-          child: InkWell(
-              onTap: () async {
-                await dataProvider.loadBoook(books[index].id);
-                Navigator.pop(context);
-              },
-              child: SizedBox(
-                  height: 50, child: Center(child: Text(books[index].name)))),
-        );
+        return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 2),
+            child: Card(
+              clipBehavior: Clip.hardEdge,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0)),
+              child: InkWell(
+                  splashColor: Colors.green.withAlpha(30),
+                  onTap: () async {
+                    await dataProvider.loadBoook(books[index].id);
+                    Navigator.pop(context);
+                  },
+                  child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Row(children: [
+                        const SizedBox(width: 10),
+                        const Icon(Icons.folder_outlined,
+                            color: Color(0xFF37745B)),
+                        const SizedBox(width: 10),
+                        Text(books[index].name)
+                      ]))),
+            ));
       },
     );
   }
